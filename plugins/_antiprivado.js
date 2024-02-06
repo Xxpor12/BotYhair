ae7426f39d85c59b33b1da936f805fa226ff9112
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-06 21:56:48 +0100

    𝗧𝗲𝗮𝗺 𝗛𝘆𝗱𝗿𝗮 𝗕𝗼𝘁

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,35 +1,13 @@
-const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, usedPrefix, command }) {
-if (m.isBaileys && m.fromMe) return !0
-if (m.isGroup) return !1
-if (!m.message) return !0
-const regex = new RegExp(`^${comandos.source}$`, 'i')
-if (regex.test(m.text.toLowerCase().trim())) return !0
-
-let chat, user, bot, mensaje
-chat = global.db.data.chats[m.chat]
-user = global.db.data.users[m.sender]
-bot = global.db.data.settings[this.user.jid] || {}
-
-if (bot.antiPrivate && !isOwner && !isROwner) {
-if (user.counterPrivate === 0) {
-//mensaje = `*@${m.sender.split`@`[0]} 𝙀𝙎𝙏𝘼 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 𝘿𝙀𝙇 𝘽𝙊𝙏.*\n\n⚠️ \`\`\`𝐏𝐑𝐈𝐌𝐄𝐑𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
-await conn.reply(m.chat, mid.smsprivado(sender), m, { mentions: [m.sender] })  
-  
-} else if (user.counterPrivate === 1) {
-let grupos = [ nn, nnn, nnnt, nnntt, nnnttt ].getRandom()
-//mensaje = `*@${m.sender.split`@`[0]} 𝙎𝙀 𝘼 𝙈𝙀𝙉𝘾𝙄𝙊𝙉𝘼𝘿𝙊 𝘼𝙉𝙏𝙀𝙍𝙄𝙊𝙍𝙈𝙀𝙉𝙏𝙀 𝙌𝙐𝙀 𝙉𝙊 𝙎𝙀 𝙇𝙀 𝙋𝙐𝙀𝘿𝙀 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝘽𝙊𝙏 𝙀𝙉 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 🫤*\n\n👇 𝙋𝙐𝙀𝘿𝙀 𝙐𝙉𝙄𝙍𝙎𝙀 𝘼 𝙉𝙐𝙀𝙎𝙏𝙍𝙊 𝙂𝙍𝙐𝙋𝙊 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙀𝙇 𝘽𝙊𝙏\n\n${grupos}\n\n𝙎𝙄 𝙑𝙐𝙀𝙇𝙑𝙀 𝘼 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 𝘿𝙀𝙇 𝘽𝙊𝙏,𝙎𝙀𝙍𝘼́ 𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝘿𝙊‼️\n\n⚠️ \`\`\`𝐒𝐄𝐆𝐔𝐍𝐃𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
-await conn.reply(m.chat, mid.smsprivado(sender, grupos), m, { mentions: [m.sender] }) 
-  
-} else if (user.counterPrivate === 2) {
-//mensaje = `*@${m.sender.split`@`[0]} 𝙎𝙀𝙍𝘼́́ 𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝘿𝙊(𝘼) 𝘿𝙀𝙇 𝘽𝙊𝙏. 😾 𝙎𝙀 𝙈𝙀𝙉𝘾𝙄𝙊𝙉𝙊́ 𝘼𝙉𝙏𝙀𝙍𝙄𝙊𝙍𝙈𝙀𝙉𝙏𝙀 𝙌𝙐𝙀 𝙉𝙊 𝙎𝙀 𝙋𝙊𝘿𝙄𝘼 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊.*\n\n⚠️ \`\`\`𝐓𝐄𝐑𝐂𝐄𝐑𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
-await conn.reply(m.chat, mid.smsprivado(sender), m, { mentions: [m.sender] }) 
-  
-user.counterPrivate = -1
-await this.updateBlockStatus(m.sender, 'block')
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`*𝗛𝗼𝗹𝗮 @${m.sender.split`@`[0]}, 𝗗𝗲𝗷𝗮𝗺𝗲 𝗗𝗲𝗰𝗶𝗿𝘁𝗲 𝗤𝘂𝗲 𝗘𝘀𝘁𝗮 𝗧𝗼𝘁𝗮𝗹𝗺𝗲𝗻𝘁𝗲 𝗣𝗿𝗼𝗵𝗶𝗯𝗶𝗱𝗼 𝗛𝗮𝗯𝗹𝗮𝗿𝗻𝗲 𝗔𝗹 𝗣𝘃 :𝟯 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗢 𝗔𝗱𝗾𝘂𝗶𝗿𝗶𝗿𝗺𝗲 𝗣𝗮𝗿𝗮 𝗧𝘂 𝗚𝗿𝘂𝗽𝗼 𝗘𝘀𝗰𝗿𝗶𝗯𝗲 𝗔𝗹 : +𝟱𝟭 𝟵𝟯𝟲 𝟵𝟵𝟰 𝟭𝟱𝟱*`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+  }
+  return !1;
 }
-user.counterPrivate++
-}
-return !1
-}
-
