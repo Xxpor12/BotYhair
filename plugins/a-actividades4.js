39d4703aa7546c5b58defcf180b98aa190d7bf5a
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-06 16:17:50 -0500

    𝗧𝗲𝗮𝗺 𝗛𝘆𝗱𝗿𝗮 𝗕𝗼𝘁
    𝗧𝗲𝗮𝗺 𝗛𝘆𝗱𝗿𝗮 𝗕𝗼𝘁

---
+++ plugins/a-actividades4.js
@@ -0,0 +1,79 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Actividades4);
+};
+handler.command = /^(Actividades4|Actividades4)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.Actividades4 = ` 
+*ACTIVIDADES HYDRA*
+
+*Mod :* 
+*Jugadores :* 
+*Fecha :* */*
+*Hora :* * 🇵🇪  🇦🇷*
+
+• 
+• 
+• 
+• 
+
+*Suplentes*
+
+• 
+• 
+
+*Donador De Sala O Sorteo :*
+
+•
+
+*"PC : "*
+*"TAG : "*
+--------------‐-----‐------------------
+*Modalidad :* 
+*Jugadores :* 
+*Fecha :* */*
+*Hora :* *🇵🇪  🇦🇷*
+
+• 
+• 
+• 
+• 
+
+
+*Suplentes*
+
+• 
+•
+
+*Donador De Sala O Sorteo :*
+
+•
+
+*"PC : "*
+*"TAG : "*
+---------------‐-----‐------------------
+*Modalidad :* 
+*Jugadores :* 
+*Fecha :* */*
+*Hora :* *🇵🇪 🇦🇷*
+
+• 
+• 
+• 
+• 
+
+*Suplentes*
+
+• 
+•
+
+*Donador De Sala O Sorteo :*
+
+•
+
+*"PC : "*
+*"TAG : "*
+`;
