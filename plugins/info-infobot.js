95f3a40f1d1b0818812cd804d19afbe6d687f952
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-07 17:17:16 +0100

    𝗧𝗲𝗮𝗺 𝗛𝘆𝗱𝗿𝗮 𝗕𝗼𝘁

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -1,125 +1,87 @@
-import os from 'os'
-import util from 'util'
-import ws from 'ws';
-import sizeFormatter from 'human-readable'
-let MessageType =  (await import(global.baileys)).default
-
-import fs from 'fs'
+import db from '../lib/database.js'
+import { cpus as _cpus, totalmem, freemem, platform, hostname, version, release, arch } from 'os'
+import speed from 'performance-now'
 import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+
+let format = sizeFormatter({
+    std: 'JEDEC',
+    decimalPlaces: 2,
+    keepTrailingZeroes: false,
+
+    
+})
 let handler = async (m, { conn, usedPrefix }) => {
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime) 
-let totalreg = Object.keys(global.db.data.users).length
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
-const groups = chats.filter(([id]) => id.endsWith('@g.us'))
-const used = process.memoryUsage()
-const cpus = os.cpus().map(cpu => {
-    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-    return cpu
-  })
-const cpu = cpus.reduce((last, cpu, _, { length }) => {
-    last.total += cpu.total
-    last.speed += cpu.speed / length
-    last.times.user += cpu.times.user
-    last.times.nice += cpu.times.nice
-    last.times.sys += cpu.times.sys
-    last.times.idle += cpu.times.idle
-    last.times.irq += cpu.times.irq
-    return last
-  }, {
-    speed: 0,
-    total: 0,
-    times: {
-      user: 0,
-      nice: 0,
-      sys: 0,
-      idle: 0,
-      irq: 0
-    }
-  })
-const { restrict } = global.db.data.settings[conn.user.jid] || {}
-const { autoread } = global.opts
-let pp = './media/menus/Menu1.jpg'
-let vn = './media/infobot.mp3'
-let grupos = [nna, nn, nnn, nnntt]
-let gata = [img5, img6, img7, img8, img9]
-let enlace = { contextInfo: { externalAdReply: {title: wm + ' ', body: 'support group' , sourceUrl: grupos.getRandom(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
-let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: 'The LoliBot-MD', thumbnailUrl: await(await fetch(img)).buffer(), sourceUrl: yt }}}
-let dos = [enlace, enlace2]
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let old = performance.now()
-  //await m.reply('_Realizando test_')
-  let neww = performance.now()
-  let totaljadibot = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
-  let speed = neww - old
+   let bot = global.db.data.settings[conn.user.jid]
+   let _uptime = process.uptime() * 1000
+   let uptime = (_uptime).toTimeString()
+   let totalreg = Object.keys(global.db.data.users).length
+   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+   const used = process.memoryUsage()
+   const cpus = _cpus().map(cpu => {
+      cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+      return cpu
+   })
+   const cpu = cpus.reduce((last, cpu, _, { length }) => {
+      last.total += cpu.total
+      last.speed += cpu.speed / length
+      last.times.user += cpu.times.user
+      last.times.nice += cpu.times.nice
+      last.times.sys += cpu.times.sys
+      last.times.idle += cpu.times.idle
+      last.times.irq += cpu.times.irq
+      return last
+   }, {
+      speed: 0,
+      total: 0,
+      times: {
+         user: 0,
+         nice: 0,
+         sys: 0,
+         idle: 0,
+         irq: 0
+      }
+   })
+   let _muptime
+   if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+         process.once('message', resolve)
+         setTimeout(resolve, 1000)
+      }) * 1000
+   }
+   let timestamp = speed()
+   let latensi = speed() - timestamp
+   let teks = ` –  *I N F O  B O T*
 
-let info = ``
-await conn.sendFile(m.chat, gataImg.getRandom(), 'lp.jpg', info, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})
-//conn.sendFile(m.chat, pp, 'gata.mp4', info, fkontak)
-/*let info = `
-╭━━━━[ ${gt} ]━━━━━⬣
-┃
-┃➥ *CREADORA | CREATOR*
-┃ღ *𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *CONTACTO | CONTACT* 
-┃ღ *${ig}*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃𓃠 *VERSIÓN ACTUAL | VERSION*
-┃ღ ${vs}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *PREFIJO | PREFIX*
-┃ღ *${usedPrefix}*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *CHATS PRIVADOS | PRIVATE CHAT*
-┃ღ *${chats.length - groups.length}*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *CHATS DE GRUPOS | GROUP CHAT*
-┃ღ *${groups.length}* 
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *CHATS EN TOTAL | TOTAL CHATS*
-┃ღ *${chats.length}* 
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *ACTIVIDAD | ACTIVITY*
-┃ღ *${uptime}*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *USUARIOS | USERS*
-┃ღ *${totalreg}* 
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *VELOCIDAD | SPEED*
-┃ღ  *${speed}*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *BOT SECUNDARIOS ACTIVOS | ACTIVE SECONDARY BACKS*
-┃ღ *${totaljadibot.length}*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *BATERIA | DRUMS*
-┃ღ *${conn.battery ? `${conn.battery.value}%* *${conn.battery.live ? '🔌 Cargando...*' : '⚡ Desconectado*'}` : 'Desconocido*'}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *AUTOREAD*
-┃ღ ${autoread ? '*Activado ✔*' : '*Desactivado ✘*'}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃➥ *RESTRICT* 
-┃ღ ${restrict ? '*Activado ✔*' : '*Desactivado ✘*'} 
-┃
-╰━━━[ 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣 ]━━⬣`.trim()
+┌  ✩  *Creador* : @${owner[0][0].split('@s.whatsapp.net')[0]}
+│  ✩  *Prefijo* : [  ${usedPrefix}  ]
+│  ✩  *Plataforma* : ${platform()}
+│  ✩  *Servidor* : ${hostname()}
+│  ✩  *RAM* : ${format(totalmem() - freemem())} / ${format(totalmem())}
+│  ✩  *FreeRAM* : ${format(freemem())}
+│  ✩  *Speed* : ${latensi.toFixed(4)} ms
+│  ✩  *Uptime* : ${uptime}
+│  ✩  *Modo* : ${bot.public ? 'Privado' : 'Publico'}
+└  ✩  *Registrados* : ${totalreg} Usuarios
 
-conn.sendHydrated(m.chat, info, wm, pp, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙑𝙚𝙧 𝙂𝙧𝙪𝙥𝙤𝙨 | 𝙎𝙚𝙚 𝙂𝙧𝙤𝙪𝙥𝙨', '#grupolista'],
-['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨', '/cuentasgb'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪', '.menu']
-], m,)*/
-//conn.reply(m.chat, info, m)
+
+ –  *I N F O  C H A T*
+
+┌  ✩  *${groupsIn.length}* Chats en Grupos
+│  ✩  *${groupsIn.length}* Grupos Unidos
+│  ✩  *${groupsIn.length - groupsIn.length}* Grupos Salidos
+│  ✩  *${chats.length - groupsIn.length}* Chats Privados
+└  ✩  *${chats.length}* Chats Totales
+
+*≡  _NodeJS Uso de memoria_*
+${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}`
+await conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [owner[0][0] + '@s.whatsapp.net'], externalAdReply: { mediaUrl: false, mediaType: 1, description: false, title: '↷✦╎Info - Bot╎⭐˖ ⸙',body: false, previewType: 0, thumbnail: miniurl, sourceUrl: ''}}})
 }
-handler.help = ['infobot']
-handler.tags = ['info', 'tools']
-handler.command = /^(infobot|informacionbot|infogata|informacióngata|informaciongata)$/i
-export default handler
 
-function clockString(ms) {
-let h = Math.floor(ms / 3600000)
-let m = Math.floor(ms / 60000) % 60
-let s = Math.floor(ms / 1000) % 60
-console.log({ms,h,m,s})
-return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['info', 'infobot']
+
+export default handler
