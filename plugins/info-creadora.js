fb35441de4065979116bea8d4be806b0a91b1b50
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-07 17:33:31 +0100

    𝗧𝗲𝗮𝗺 𝗛𝘆𝗱𝗿𝗮 𝗕𝗼𝘁

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -9,7 +9,6 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 let pp = gataImg.getRandom()	
 const cat = `*Hola Si Deseas Hablarle A los Lideres Y Ceos Del Team Hydra Estos Son Sus Numeros :* 
 *1 :* +51 936 994 155 *Ceo Hydra*
-*2 :* +51 980 269 276 *Co Ceo Hydra*
 *3 :* +51 978 420 752 *Co Lider Hydra*
 *4 :* +54 9 291 513 9790 *Decana Hydra*
 *3 :* +51 963 181 293 *Decano Hydra*
