6851472ab6d71259d4031afbee4398bf809292b6
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-09 20:24:31 +0100

    𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -4,8 +4,8 @@ const handler = async (m, { conn, text }) => {
   try {
           const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
           let messager = stdout.toString()
-          if (messager.includes('Already up to date.')) messager = '*[❗] No hay nada por actualizar, todo esta actualizado a como esta en el repositorio oficial.*'
-          if (messager.includes('Updating')) messager = '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕\n\n' + stdout.toString()
+          if (messager.includes('Already up to date.')) messager = '𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁'
+          if (messager.includes('Updating')) messager = '𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁\n\n' + stdout.toString()
           conn.reply(m.chat, messager, m);
   } catch {      
  try {    
