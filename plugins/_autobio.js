919bf99f1131978e07fbe5cab2cb6667de61252d
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-09 19:58:13 +0100

    𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `𝗧𝗲𝗮𝗺 𝗛𝘆𝗱𝗿𝗮 𝗕𝗼𝘁 𝗘𝗹 𝗠𝗲𝗷𝗼𝗿 𝗗𝗲 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ║ 𝗧𝗶𝗲𝗺𝗽𝗼 𝗔𝗰𝘁𝗶𝘃𝗼 ${uptime}`
+let bio = `𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁 𝗘𝗹 𝗠𝗲𝗷𝗼𝗿 𝗗𝗲 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ║ 𝗧𝗶𝗲𝗺𝗽𝗼 𝗔𝗰𝘁𝗶𝘃𝗼 ${uptime}`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
