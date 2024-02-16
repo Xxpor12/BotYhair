887d456bf32ab37fb463ec5424422c6b40a6eac4
LongBot14 <159817076+LongBot14@users.noreply.github.com>
2024-02-16 23:44:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Ay\nPapi\nKchame', {
+say('Long\nTime', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`OPTIMUS BOT`, {
+say(`Long Bot`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
